9fe4308dcb7b966fa7ae37d53670b0bc60f25f7e
haq2682 <haq2682@gmail.com>
2024-03-15 10:20:58 +0500

    Day 4 Challenge

---
+++ Arrays/Container With Most Water.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} height
+ * @return {number}
+ */
+var maxArea = function(height) {
+    let left = 0;
+    let right = height.length - 1;
+    let area = null;
+    let maxArea = null;
+
+    while(left < right) {
+        area = Math.min(height[left], height[right]) * (right-left);
+        maxArea = Math.max(maxArea, area);
+        if(height[left] < height[right]) left++;
+        else right--;
+    }
+    return maxArea;
+};
