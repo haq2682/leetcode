950b3e468a24ce852858d38f312e9b21c0a7f025
haq2682 <haq2682@gmail.com>
2024-03-20 11:04:57 +0500

    Day 9 Challenge

---
+++ DFS/Validate BST.js
@@ -0,0 +1,24 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+var isValidBST = function(root) {
+    let previousNode = null;
+    function dfs(node) {
+        if(!node) return true;
+        var resultFromRight = dfs(node.right);
+        if(!resultFromRight) return false;
+        if(previousNode && node.val > previousNode.val) return false;
+        previousNode = node;
+        return dfs(node.left);
+    }
+    return dfs(root);
+};
